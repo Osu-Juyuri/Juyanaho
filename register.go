ec0445cec8c88f34637ae84410e0fb80bfd7c99a
Alicia <contact@ilyt.cc>
2019-02-23 16:25:28 +0000

    ?

--- register.go
+++ register.go
@@ -94,8 +94,9 @@ func registerSubmit(c *gin.Context) {
 		return
 	}
 
-	if db.QueryRow("SELECT 1 FROM beta_keys WHERE key_md5 = ?", cmd5(c.PostForm("key"))).
+	if db.QueryRow("SELECT 1 FROM beta_keys WHERE key_md5 = ? AND allowed = 1", cmd5(c.PostForm("key")))
 		Scan(new(int)) != sql.ErrNoRows {
+		fmt.Println(db.QueryRow("SELECT 1 FROM beta_keys WHERE key_md5 = ? AND allowed = 1", cmd5(c.PostForm("key"))))
 		registerResp(c, errorMessage{T(c, "Invalid Beta Key.")})
 		return
 	}
